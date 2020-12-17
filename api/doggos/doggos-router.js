47d8a4d308fc9c4a206f3e474022c28d2b466a92
April Darger <april.m.darger@gmail.com>
2020-12-17 16:55:56 -0700

    Imported middleware and wrote POST and DELETE endpoints.

--- api/doggos/doggos-router.js
+++ api/doggos/doggos-router.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const Doggos = require('./doggos-model');
+const Doggo = require('./doggos-model');
+const { realDoggo, validateId } = require('../middleware/middleware');
 
-router.post('/', (req, res) => {
-  
+router.post('/', realDoggo, async (req, res) => {
+  try{
+    const doggo = req.body;
+    const magic = Doggo.create(doggo);
+    res.json(magic);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateId, async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Doggo.remove(id);
+    res.status(200).json({ message:req.doggo });
+  } catch (error) {
+    res.status(500).json({ message:error.message });
+  }
 });
 
 module.exports = router;
