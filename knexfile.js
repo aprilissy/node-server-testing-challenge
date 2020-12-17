6a620236ae801644df13ea41125e85486bb8c1b4
April Darger <april.m.darger@gmail.com>
2020-12-17 13:52:50 -0700

    Set up knexfile.js for development and testing environment with sharedConfig for DRY code.

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,22 @@
-// Update with your config settings.
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' }
+}
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    ...sharedConfig,
     connection: {
       filename: './dev.sqlite3'
     }
   },
-
-  staging: {
-    client: 'postgresql',
+  testing: {
+    ...sharedConfig,
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      filename: './dev.sqlite3'
     }
   }
-
 };
